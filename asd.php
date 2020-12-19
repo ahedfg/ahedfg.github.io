3adfc4a15cc10d232423b425ecad7869bc8db03d
ahedfg <74929731+ahedfg@users.noreply.github.com>
2020-12-19 12:20:24 +0900

    Add files via upload

---
+++ asd.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>result</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body>
+  <div class="result-head">
+    <h1 class="main-title ng-binding">Your excrement..</h1>
+  </div>
+
+  <div class="result-container">
+    <h1 class="area">can support annually<?php echo $_REQUEST['slider'];?></h1>
+  </div>
+</body>
+</html>
